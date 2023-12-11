470f9abf4f8c19b280a7510f9b86c09961b6dd0a
Nima Pourjafar <nima.pourjafar123@gmail.com>
2023-12-11 15:37:48 -0500

    added bio

--- index.js
+++ index.js
@@ -18,26 +18,25 @@ document.addEventListener("DOMContentLoaded", function () {
     // Clear the canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-
     for (let i = 0; i < num_lines; i++) {
-      lx1 = x1(t+i)*4 + shiftX;
-      ly1 = y1(t+i)*4 + shiftY;
-      lx2 = x2(t+i)*4 + shiftX;
-      ly2 = y2(t+i)*4 + shiftY;
+      lx1 = x1(t + i) * 4 + shiftX;
+      ly1 = y1(t + i) * 4 + shiftY;
+      lx2 = x2(t + i) * 4 + shiftX;
+      ly2 = y2(t + i) * 4 + shiftY;
 
       ctx.beginPath();
       ctx.moveTo(lx1, ly1);
       ctx.lineTo(lx2, ly2);
-      var hue = ((t+i) / 3) % 360; // Change hue value over time
-      ctx.strokeStyle = `hsla(${hue}, 95%, 75%, 33%)`; // Set stroke color
-      ctx.lineWidth = 1.25; // Define line width
+      var hue = ((t + i) / 3) % 360; // Change hue value over time
+      //   ctx.strokeStyle = `hsla(${hue}, 95%, 75%, 33%)`; // Set stroke color
+    //   ctx.strokeStyle = `rgba(0,0,0,${t+i})`;
+    ctx.strokeStyle = 'black'
+      ctx.lineWidth = 0.5; // Define line width
       ctx.stroke();
       ctx.closePath();
     }
 
-
     t += 0.2;
-
 
     // Request the next frame
     requestAnimationFrame(draw);
@@ -48,16 +47,15 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 const x1 = (t) => {
-    return Math.sin(t / 10) * 100 + Math.sin(t / 5) * 20;
-  };
-  const y1 = (t) => {
-    return Math.cos(t / 10) * 100;
-  };
-  
-  const x2 = (t) => {
-    return Math.sin(t / 10) * 200 + Math.sin(t) * 2;
-  };
-  const y2 = (t) => {
-    return Math.sin(t / 20) * 200 + Math.cos(t / 12) * 20;
-  };
-  
+  return Math.sin(t / 10) * 100 + Math.sin(t / 5) * 20;
+};
+const y1 = (t) => {
+  return Math.cos(t / 10) * 100;
+};
+
+const x2 = (t) => {
+  return Math.sin(t / 10) * 200 + Math.sin(t) * 2;
+};
+const y2 = (t) => {
+  return Math.sin(t / 20) * 200 + Math.cos(t / 12) * 20;
+};
