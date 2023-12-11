ddeee460fb07d3e1c91273001ae05507a1b773e4
Nima Pourjafar <nima.pourjafar123@gmail.com>
2023-12-10 22:06:02 -0500

    added sick line effect

---
+++ index.js
@@ -0,0 +1,61 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var canvas = document.getElementById("background-canvas");
+  var ctx = canvas.getContext("2d");
+
+  // Set canvas width and height to cover the entire screen
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  var dotRadius = 10;
+  var dotColor = "black";
+  var shiftX = canvas.width / 2; // Center dot horizontally
+  var shiftY = canvas.height / 2; // Center dot vertically
+
+  var t = 0; // Initialize the value of t
+  var h = 0;
+  var num_lines = 20;
+
+  function draw() {
+    // Clear the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+
+    for (let i = 0; i < num_lines; i++) {
+      lx1 = x1(t+i)*3 + shiftX;
+      ly1 = y1(t+i)*3 + shiftY;
+      lx2 = x2(t+i)*3 + shiftX;
+      ly2 = y2(t+i)*3 + shiftY;
+
+      ctx.beginPath();
+      ctx.moveTo(lx1, ly1);
+      ctx.lineTo(lx2, ly2);
+      ctx.strokeStyle = "black"; // Define line color
+      ctx.lineWidth = 2; // Define line width
+      ctx.stroke();
+      ctx.closePath();
+    }
+
+    // Increment t for the next frame
+    t += 0.3;
+
+    // Request the next frame
+    requestAnimationFrame(draw);
+  }
+
+  // Start the animation loop
+  draw();
+});
+
+const x1 = (t) => {
+  return Math.sin(t / 10) * 100 + Math.sin(t / 5) * 20;
+};
+const y1 = (t) => {
+  return Math.cos(t / 10) * 100;
+};
+
+const x2 = (t) => {
+  return Math.sin(t / 10) * 200 + Math.sin(t) * 2;
+};
+const y2 = (t) => {
+  return Math.sin(t / 20) * 200 + Math.cos(t / 12) * 20;
+};
